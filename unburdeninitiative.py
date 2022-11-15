2b8c2d797df756e964e24d915e2dcab09b452417
unburdeninitiative <117644954+unburdeninitiative@users.noreply.github.com>
2022-11-15 22:24:09 +0300

    Update unburdeninitiative.py

--- unburdeninitiative.py
+++ unburdeninitiative.py
@@ -2,18 +2,18 @@ from flask import Flask, flash, redirect, render_template, request, session
 
 app= Flask(__name__)
 
-@app.route("/")
+@app.route("/", endpoint='home')
 def home():
     return render_template("homepage.html")
 
-@app.route("/About_us")
+@app.route("/About_us",endpoint='about')
 def about():
     return render_template("Aboutus.html")
 
-@app.route("/Donate")
-def about():
+@app.route("/Donate",endpoint='donate')
+def donate():
     return render_template("Donate.html")
 
-@app.route("/Contact")
-def about():
+@app.route("/Contact",endpoint='contact')
+def contact():
     return render_template("Contact.html")
